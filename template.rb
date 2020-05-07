5b0d74a32ed00870613cbae2a5e5fcab441b5254
Paul Keen <62-pkeen@users.noreply.rtfm.corsis.com>
2020-05-07 19:46:28 +0300

    Adds default test helpers

--- template.rb
+++ template.rb
@@ -2,10 +2,25 @@
 
 say 'Updating rails to run minitests/spec'
 
+source_paths.unshift(__dir__)
+
 gem 'minitest-spec-rails', require: false, group: :test
 gem 'rspec-expectations', require: false, group: :test
+gem 'capybara', require: false, group: :test
 
 `bundle install`
+
+directory 'lib/install/test', 'test'
+
+insert_into_file('test/test_helper.rb', <<~TEST_HELPER_SNIPPET, before: 'class ActiveSupport::TestCase')
+
+  require 'minitest-spec-rails'
+  require 'rspec/expectations/minitest_integration'
+
+  require_relative 'support/shared_examples'
+
+TEST_HELPER_SNIPPET
+
 
 say <<~MSG
   **************************
