08cad9b25c828fb281462c44754e2aa93db20209
Paul Keen <62-pkeen@users.noreply.rtfm.corsis.com>
2020-05-07 03:45:33 +0300

    Adds migration impl

--- lib/r2m/command.rb
+++ lib/r2m/command.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require 'thor'
+require 'pathname'
+
 require_relative './spec_convector'
+require_relative './spec_migration'
 
 module R2M
   # Adds CLI commands convert and migrate
@@ -15,31 +18,40 @@ module R2M
       true
     end
 
-    desc 'convert [path...]', 'Convert Rspec to minitest'
-
-    def convert(pathes)
-      files(pathes).each { |file| process file }
+    desc 'convert [path1 path2 ...]', 'Convert Rspec to minitest'
+    def convert(*paths)
+      files(paths.flatten).each { |path| run_convert path }
     end
 
-    desc 'migrate [path...]', 'Move found specs to test folder and convert them'
-    method_option :replace_suffix,
-                  desc: 'Renames *_spec.rb to *_test.rb',
-                  default: true,
-                  aliases: ['s'],
-                  type: :boolean
-
-    def migrate(pathes)
-      pp files(pathes)
+    desc 'migrate [path1 path2 ...]', 'Move found specs to test folder and convert them'
+    method_option(
+      :replace_suffix,
+      desc: 'Renames *_spec.rb to *_test.rb',
+      default: true,
+      aliases: ['s'],
+      type: :boolean
+    )
+    def migrate(*paths)
+      files(paths.flatten).each do |path|
+        say "Processing #{path}"
+        run_migrate(path)
+        run_convert(path)
+      end
     end
 
     private
 
-    def process(file)
+    def run_migrate(path)
+      SpecMigration.new(Pathname.pwd).migrate(File.realpath(path))
+    end
+
+    def run_convert(file)
+      say "Processing #{file}"
       SpecConvector.new(self).process(file)
     end
 
-    def files(pathes)
-      Array(pathes).map do |path|
+    def files(paths)
+      Array(paths).map do |path|
         if File.exist?(path) && !File.directory?(path)
           path
         elsif Dir.exist?(path)
